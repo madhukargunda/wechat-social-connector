5bd5596c4ae4deca7bbe2df01b644771fadd35e7
Madhukar Gunda <madhukargunda@Madhukars-MacBook-Pro.local>
2020-06-07 22:50:35 +0800

    Dockerizing the spring boot war with tomcat

--- wechat-social-connector/src/main/java/study/pattern/social/connector/SocialConnectorApplication.java
+++ wechat-social-connector/src/main/java/study/pattern/social/connector/SocialConnectorApplication.java
@@ -2,12 +2,19 @@ package study.pattern.social.connector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SocialConnectorApplication {
+public class SocialConnectorApplication extends SpringBootServletInitializer  {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialConnectorApplication.class, args);
